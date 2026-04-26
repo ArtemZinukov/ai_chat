996c274cb36a810145f5159e97dd4b8f69e79f47
артем зинуков <zin1211@yandex.ru>
2026-04-26 17:12:09 +0300

    Init project

---
+++ main.py
@@ -0,0 +1,71 @@
+import aiofiles
+from contextlib import asynccontextmanager, suppress
+from environs import Env
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+from starlette.websockets import WebSocketDisconnect
+from gigachat import GigaChat
+from gigachat.models import Chat, Messages, MessagesRole
+
+env = Env()
+env.read_env()
+
+GIGA_CREDS = env.str("GIGACHAT_CREDENTIALS")
+GIGA_SCOPE = env.str("GIGACHAT_SCOPE", "GIGACHAT_API_PERS")
+GIGA_MODEL = env.str("GIGACHAT_MODEL", "GigaChat")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with GigaChat(
+        credentials=GIGA_CREDS,
+        scope=GIGA_SCOPE,
+        verify_ssl_certs=False
+    ) as giga:
+        app.state.giga_client = giga
+        yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+async def ask_giga(client: GigaChat, prompt: str) -> str:
+    payload = Chat(
+        messages=[Messages(role=MessagesRole.USER, content=prompt)],
+        model=GIGA_MODEL
+    )
+    response = await client.achat(payload)
+    return response.choices[0].message.content
+
+async def read_file(file):
+    async with aiofiles.open(file, mode="r", encoding="utf-8") as f:
+        return await f.read()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index():
+    return await read_file("index.html")
+
+def get_giga_client():
+    return app.state.giga_client
+
+@app.websocket("/ws/chat")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    giga: GigaChat = get_giga_client()
+
+    with suppress(WebSocketDisconnect):
+        try:
+            while True:
+                data = await websocket.receive_json(mode='text')
+                user_prompt = data.get("content")
+
+                if user_prompt:
+                    answer = await ask_giga(giga, user_prompt)
+
+                    await websocket.send_json({
+                        "type": "ai_response_chunk",
+                        "content": answer
+                    })
+        finally:
+            with suppress(RuntimeError):
+                await websocket.close()
